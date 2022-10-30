9f9947956b39b45f23fb4989b3f44ec277274783
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-30 08:50:18 +0100

    feat: add string concatenation demo and benchmarks

---
+++ Working with Strings/StringConcatenationComparisons.Benchmark/StringConcatenationComparison.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace StringConcatenationComparisons.Benchmark;
+
+[MemoryDiagnoser]
+public class StringConcatenationComparison
+{
+    private const string StringToConcat = "Hello, World!";
+
+    [Params(5, 10, 50, 100, 1000, 5000, 100_000)]
+    public int Times { get; set; }
+
+    [Benchmark]
+    public string StringConcatenation()
+    {
+        return Concat(string.Empty, StringToConcat, Times);
+    }
+
+    [Benchmark]
+    public string StringBuilderConcatenation()
+    {
+        return Concat(new StringBuilder(), StringToConcat, Times);
+    }
+
+    private static string Concat(string source, string stringToConcat, int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            source = string.Concat(source, stringToConcat);
+        }
+
+        return source;
+    }
+
+    private static string Concat(StringBuilder source, string stringToConcat, int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            source.Append(stringToConcat);
+        }
+
+        return source.ToString();
+    }
+}
---
+++ Working with Strings/StringConcatenationComparisons.Demo/StringConcatinationTests.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace StringConcatenationComparisons.Demo;
+
+[TestFixture]
+public class StringConcatinationTests
+{
+    private const char CharToConcat = 'A';
+    private const int BigNumberOfAppends = 20000000;
+
+    [TestCaseSource(nameof(SmallTestData))]
+    public void StringConcatenation_ShortData(int numberOfCharacters)
+    {
+        string result = string.Empty;
+        for (int i = 0; i < numberOfCharacters; i++)
+        {
+            result += CharToConcat;
+        }
+    }
+
+    [TestCaseSource(nameof(SmallTestData))]
+    public void StringBuilderConcatenation_ShortData(int numberOfCharacters)
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < numberOfCharacters; i++)
+        {
+            result.Append(CharToConcat);
+        }
+    }
+
+    [Test, Timeout(20000)]
+    public void StringConcatenation_LongData()
+    {
+        string result = string.Empty;
+        for (int i = 0; i < BigNumberOfAppends; i++)
+        {
+            result += CharToConcat;
+        }
+    }
+
+    [Test]
+    public void StringBuilderConcatenation_LongData()
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < BigNumberOfAppends; i++)
+        {
+            result.Append(CharToConcat);
+        }
+    }
+
+    private static IEnumerable<int> SmallTestData
+    {
+        get
+        {
+            yield return 5;
+
+            yield return 10;
+
+            yield return 50;
+
+            yield return 100;
+
+            yield return 500;
+
+            yield return 1000;
+
+            yield return 5000;
+        }
+    }
+}

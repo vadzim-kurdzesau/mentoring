88684fc10173fd4e1ff8304f13461e25aa29bc18
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-30 08:50:29 +0100

    feat: add string parsing demo

---
+++ Working with Strings/StringParsing.Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StringParsing.Demo;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        const string message = "Hello, World!";
+        if (args.Length != 2)
+        {
+            Console.WriteLine("The number of arguments must equal 2.");
+            return;
+        }
+
+        string numberOfTimesString = args[0], shouldUpperCaseString = args[1];
+
+        // TryParse, out parameters
+        bool isInteger = int.TryParse(numberOfTimesString, out int numberOfMessages);
+        if (!isInteger)
+        {
+            Console.WriteLine("First parameter must be an integer.");
+            return;
+        }
+
+        bool shouldUpperCase = bool.Parse(shouldUpperCaseString);
+
+        for (int i = 0; i < numberOfMessages; i++)
+        {
+            Console.WriteLine(shouldUpperCase ? message.ToUpper() : message);
+        }
+    }
+}

71abb89a12e79192dcbba32ba6b55513fb3a97a0
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-30 08:46:30 +0100

    feat: add strings demo

--- Working with Strings/WorkingWithStrings.Demo/Program.cs
+++ Working with Strings/WorkingWithStrings.Demo/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WorkingWithStrings.Demo;
+
+public class Program
+{
+    private static void Main()
+    {
+        string sourceMessage = "    Hello, World!    ";
+
+        // Getting the string length
+        // String concatenation via '+' operator
+        int messageLength = sourceMessage.Length;
+        Console.WriteLine("Message length is " + messageLength);
+
+        PrintSeparationLine();
+
+        // Accessing string characters by index
+        // String concatenation via string.Concat
+        for (int i = 0; i < messageLength; i++)
+        {
+            string message = string.Concat(i, ") ", sourceMessage[i]);
+            Console.WriteLine(message);
+        }
+
+        PrintSeparationLine();
+
+        DemonstateStringTrimming(sourceMessage);
+
+        PrintSeparationLine();
+    }
+
+    private static void DemonstateStringTrimming(string sourceMessage)
+    {
+        // String trimming and string interpolation/string.Format
+        string trimmedMessage = sourceMessage.Trim();
+        Console.WriteLine($"The source message '{sourceMessage}' has been trimmed to '{trimmedMessage}'.");
+
+        string anotherMessage = "####SpamMessage###";
+        string anotherTrimmedMessage = anotherMessage.Trim('#');
+        Console.WriteLine(string.Format("The source message '{0}' has been trimmed to '{1}'.", anotherMessage, anotherTrimmedMessage));
+    }
+
+    private static void PrintSeparationLine()
+    {
+        Console.WriteLine("==============================================");
+    }
+}

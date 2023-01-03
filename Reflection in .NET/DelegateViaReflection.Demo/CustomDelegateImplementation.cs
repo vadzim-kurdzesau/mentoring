80b31ee651d949786e9ac7023e1b5be0692a6ba3
Vadzim Kurdzesau <VKurdzesau@slb.com>
2023-01-04 00:36:50 +0100

    feat: implement custom delegate

---
+++ Reflection in .NET/DelegateViaReflection.Demo/CustomDelegateImplementation.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace DelegateViaReflection.Demo
+{
+    internal class CustomDelegateImplementation
+    {
+        public object? Instance { get; set; }
+
+        required public MethodInfo Method { get; set; }
+
+        public object Invoke(params object[] arguments)
+        {
+            return Method.Invoke(Instance, arguments);
+        }
+    }
+}
---
+++ Reflection in .NET/DelegateViaReflection.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace DelegateViaReflection.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var methodToCall = typeof(Program).GetMethod("PrintMessage", BindingFlags.NonPublic | BindingFlags.Static)!;
+            var customDelegate = new CustomDelegateImplementation() { Method = methodToCall };
+            customDelegate.Invoke("Hello world!");
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
